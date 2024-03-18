7a7ec265698bfbeeb1207f9861b1fe514c4d5215
Murata Yu <muratayu@team-lab.com>
2024-03-18 23:00:30 +0900

    [update] port

--- main.go
+++ main.go
@@ -4,20 +4,32 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-  if err := run(context.Background()); err != nil {
+  if len(os.Args) != 2 {
+	log.Printf("need port number\n")
+	os.Exit(1)
+  }
+  p := os.Args[1]
+  l, err := net.Listen("tcp", ":"+p)
+  if err != nil {
+	log.Fatalf("failed to listen port %s: %v", p, err)
+  }
+  if err := run(context.Background(), l); err != nil {
 	log.Printf("failed to terminate server: %v", err)
   }
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
   s := &http.Server{
-	Addr: ":18080",
+	// 引数で受け取ったnet.Listenerを利用するので、
+	// Addrフィールドは指定しない
 	Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 	  fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	}),
@@ -25,12 +37,12 @@ func run(ctx context.Context) error {
   eg, ctx := errgroup.WithContext(ctx)
   // 別ゴルーチンでHTTPサーバーを起動する
   eg.Go(func() error {
-	// http.ErrServerClosedは
-	// http.Server.Shutdown()が正常に終了したことを示すので異常ではない。
-	if err := s.ListenAndServe(); err != nil &&
-		err != http.ErrServerClosed {
-		log.Printf("failed to close: %+v", err)
-		return err
+	if err := s.Serve(l); err != nil &&
+	  // http.ErrServerClosedは
+	  // http.Server.Shutdown()が正常に終了したことを示すので異常ではない
+	  err != http.ErrServerClosed {
+	  log.Printf("failed to close: %+v", err)
+	  return err
 	}
 	return nil
   })
