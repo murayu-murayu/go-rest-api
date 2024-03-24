cf81fde14fa6994212392933f789d6b7c5660a37
Murata Yu <muratayu@team-lab.com>
2024-03-24 17:08:06 +0900

    [update] decompose server

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
 
 	"github.com/murayu-murayu/go-rest-api/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -22,8 +19,6 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -34,32 +29,7 @@ func run(ctx context.Context) error {
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	s := &http.Server{
-		// 引数で受け取ったnet.Listenerを利用するので、
-		// Addrフィールドは指定しない
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// time.Sleep(5 * time.Second) // コマンドラインでの実験用
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	// 別ゴルーチンでHTTPサーバーを起動する
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-			// http.ErrServerClosedは
-			// http.Server.Shutdown()が正常に終了したことを示すので異常ではない
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	//チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	// Goメソッドで起動した別のゴルーチンの終了を待つ。
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
