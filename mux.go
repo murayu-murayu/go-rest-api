c583092ed116a1cc3c74143f66b9bbc1c7e5c1bb
Murata Yu <muratayu@team-lab.com>
2024-03-29 08:21:29 +0900

    [update] func nuwmux

--- mux.go
+++ mux.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
+	"github.com/murayu-murayu/go-rest-api/clock"
+	"github.com/murayu-murayu/go-rest-api/config"
 	"github.com/murayu-murayu/go-rest-api/handler"
 	"github.com/murayu-murayu/go-rest-api/store"
 )
 
-func NewMux() http.Handler {
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=utf=8")
-		// 静的解析のエラーを回避するため明示的に戻り値を捨てている
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
 	v := validator.New()
-	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	db, cleanup, err := store.New(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	r := store.Repository{Clocker: clock.RealClocker{}}
+	at := &handler.AddTask{DB: db, Repo: &r, Validator: v}
 	mux.Post("/tasks", at.ServeHTTP)
-	lt := &handler.ListTask{Store: store.Tasks}
+	lt := &handler.ListTask{DB: db, Repo: &r}
 	mux.Get("/tasks", lt.ServeHTTP)
-	return mux
+	return mux, cleanup, nil
 }
