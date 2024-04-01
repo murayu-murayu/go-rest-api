a9c46c1c755a66ce9620a35dc376986de98100ae
Murata Yu <muratayu@team-lab.com>
2024-04-01 21:23:53 +0900

    [update] refactor add_task with interface

--- handler/add_task.go
+++ handler/add_task.go
@@ -5,14 +5,11 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/jmoiron/sqlx"
 	"github.com/murayu-murayu/go-rest-api/entity"
-	"github.com/murayu-murayu/go-rest-api/store"
 )
 
 type AddTask struct {
-	DB        *sqlx.DB
-	Repo      *store.Repository
+	Service   AddTaskService
 	Validator *validator.Validate
 }
 
@@ -34,11 +31,7 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	t := &entity.Task{
-		Title:  b.Title,
-		Status: entity.TaskStatusTodo,
-	}
-	err := at.Repo.AddTask(ctx, at.DB, t)
+	t, err := at.Service.AddTask(ctx, b.Title)
 	if err != nil {
 		RespondJSON(ctx, w, &ErrResponse{
 			Message: err.Error(),
